e6e737f9d4db10fde5750036cfda1161e1e57b60
ramong26 <rlatndus21049@gmail.com>
2025-12-25 17:55:52 +0900

    feature: AuthService - 에러 조건 추가

--- src/auth/services/auth.services.ts
+++ src/auth/services/auth.services.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { UserService } from 'src/user/services/user.services';
 import * as bcrypt from 'bcrypt';
 import { Provider } from 'src/common/enums/provider.enum';
@@ -14,24 +14,36 @@ export class AuthService {
       throw new Error('Email already in use');
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await this.userService.create({
-      nickname,
-      account: {
-        accountName: nickname,
-        email,
-        password: hashedPassword,
-        passwordConfirm: '',
-        provider: Provider.LOCAL,
-        providerId: '',
-      } as Partial<Account>,
-    });
-
-    if (newUser.account) {
-      delete newUser.account.password;
+    let hashedPassword: string;
+    try {
+      hashedPassword = await bcrypt.hash(password, 10);
+    } catch {
+      throw new Error('Error hashing password');
     }
 
-    return newUser;
+    try {
+      const newUser = await this.userService.create({
+        nickname,
+        account: {
+          accountName: nickname,
+          email,
+          password: hashedPassword,
+          passwordConfirm: '',
+          provider: Provider.LOCAL,
+          providerId: '',
+        } as Partial<Account>,
+      });
+
+      if (newUser.account) {
+        delete newUser.account.password;
+      }
+
+      return newUser;
+    } catch (error) {
+      if (error.code === 'ER_DUP_ENTRY' || error.code === '23505') {
+        throw new ConflictException('이메일이 이미 등록되었습니다');
+      }
+      throw new Error('Error creating user: ' + error.message);
+    }
   }
 }
